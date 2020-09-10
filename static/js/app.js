6231ab271ada82214dc844866dcfff8959c260d4
AmyAndersson <66049569+AmyAndersson@users.noreply.github.com>
2020-09-10 18:10:12 +0800

    table and start of filter

---
+++ static/js/app.js
@@ -0,0 +1,43 @@
+// from data.js
+var tableData = data;
+
+var tbody = d3.select("tbody");
+
+var button= d3.select("button");
+
+
+// YOUR CODE HERE!
+tableData.forEach((table_enter) => {
+  var tbody = d3.select("tbody");
+    var row = tbody.append("tr");
+    Object.entries(table_enter).forEach(([key, value]) => {
+      var cell = row.append("td");
+      cell.text(value);
+    });
+    });
+
+
+
+function runEnter() {
+      // remove any children from the list to
+     d3.event.preventDefault();
+     tbody.html("")
+
+  var userinput= d3.select("#datetime");
+  var inputvalue=userinput.property('value');
+
+  var filtered= tableData.filter(filt_Date => filt_Date.datetime === inputvalue);
+ 
+  filtered.forEach((table_filt) => {
+    var tbody = d3.select("tbody");
+    var row2 = tbody.append("tr");
+    Object.entries(table_filt).forEach(([key, value]) => {
+      var cell2 = row2.append("td");
+      cell2.text(value);
+    });   
+  });
+};
+
+
+userinput.on("change", runEnter);
+button.on("click", runEnter); 
